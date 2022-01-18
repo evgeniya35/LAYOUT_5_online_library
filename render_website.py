17d864d40c26de6677591df58de9c9b5a3b908ed
evgeniya35 <evgeniya35@yandex.ru>
2022-01-18 14:29:41 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,27 +1,38 @@
+import os
 import json
 
 from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def render_books():
-    with open('books.json', 'r') as file:
-        books_json = file.read()
-    books = list(chunked(json.loads(books_json)[:10], 2))
-
+def render_page(page, books):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
-
     rendered_page = template.render(books=books)
-
-    with open('index.html', 'w', encoding='utf8') as file:
+    with open(page, 'w', encoding='utf8') as file:
         file.write(rendered_page)
 
+
+def load_books():
+    pages_folder = os.path.join(os.path.dirname(__file__), 'pages')
+    os.makedirs(pages_folder, exist_ok=True)
+
+    with open('books.json', 'r') as file:
+        books_json = file.read()
+    books = list(chunked(json.loads(books_json), 2))
+    
+    for i, books_page in enumerate(list(chunked(books, 10)), 1):
+        render_page(
+            os.path.join(pages_folder, f'index{i}.html')
+            ,books_page
+            )
+
+
 def main():
-    render_books()
+    load_books()
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -2,10 +2,10 @@ from http import server
 import re
 from livereload import Server
 
-from render_website import render_books
+from render_website import load_books
 
-render_books()
+load_books()
 
 server = Server()
-server.watch('template.html', render_books)
+server.watch('template.html', load_books)
 server.serve(root='.')
